9bd6e68fefea5b9b149d63fb945f5ea604cbe0ae
brx86 <114852137@qq.com>
2021-08-28 19:23:25 +0800

    update

---
+++ drawFans/calc.py
@@ -0,0 +1,55 @@
+import os, csv, json, time, requests
+from pyecharts import options as opts
+from pyecharts.charts import Page, Pie
+
+
+def getName(mid):
+    userApi = f"https://api.bilibili.com/x/web-interface/card?mid={mid}"
+    up = requests.get(userApi).json()["data"]
+    upName = up["card"]["name"]
+    upFans = int(up["follower"])
+    if upFans < 250:
+        upPages = int(upFans) // 50 + 1
+    else:
+        upPages = 5
+    return upName, upPages
+
+
+upMid = os.sys.argv[1]
+upDir = getName(upMid)[0]
+
+lvList = [[f"lv{x}", 0] for x in range(7)]
+with open(f"{upDir}/fans.csv", "r") as f1:
+    rCsv = csv.reader(f1)
+    for line in rCsv:
+        lv = line[4]
+        if not line[4].isdigit():
+            continue
+        lv = int(lv)
+        lvList[lv][1] += 1
+
+
+sumUsers = 0
+for u in range(7):
+    sumUsers += lvList[u][1]
+for u in range(7):
+    lvList[u][1] = round(100 * lvList[u][1] / sumUsers, 2)
+
+
+tTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time() + 30))
+pie = (
+    Pie()
+    .add("", lvList, radius=["30%", "70%"], center=["55%", "50%"])
+    .set_global_opts(
+        title_opts=opts.TitleOpts(
+            title=f"{upDir} 粉丝等级占比",
+            subtitle=f"最后更新于: {tTime}\n\nUID: {upMid}   样本数: {sumUsers}",
+            pos_left="15%",
+        ),
+        legend_opts=opts.LegendOpts(orient="vertical", pos_top="30%", pos_left="15%"),
+    )
+    .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}%"))
+)
+
+pie.render(f"{upDir}/pie.html")
+print("Chart Generated!")
---
+++ drawFans/main.py
@@ -0,0 +1,64 @@
+import os, csv, calc, time, random, requests, threadpool
+
+
+def getName(mid):
+    global session
+    userApi = f"https://api.bilibili.com/x/web-interface/card?mid={mid}"
+    up = session.get(userApi).json()["data"]
+    upName = up["card"]["name"]
+    upFans = int(up["follower"])
+    if upFans < 250:
+        upPages = int(upFans) // 50 + 1
+    else:
+        upPages = 5
+    return upName, upPages
+
+
+def getFans(mid, i):
+    global session
+    print(f"Searching page {i+1}...")
+    fansApi = f"http://api.bilibili.com/x/relation/followers?vmid={mid}&pn={i+1}"
+    return session.get(fansApi).json()["data"]["list"]
+
+
+def getInfo(u):
+    global session
+    userApi = f"http://api.bilibili.com/x/space/acc/info?mid={u['mid']}&jsonp"
+    try:
+        ui = session.get(userApi).json()["data"]
+        print(f"UID: {u['mid']}\tLevel: {ui['level']}")
+        with open(f"{up[0]}/fans.csv", "a+") as f:
+            wCsv = csv.writer(f)
+            wCsv.writerow(
+                [
+                    u["mid"],
+                    u["mtime"],
+                    u["uname"],
+                    u["vip"]["vipType"],
+                    ui["level"],
+                    ui["sex"],
+                    u["sign"].replace("\n", " "),
+                ]
+            )
+    except TypeError:
+        print("请求被拦截，请勿频繁使用!")
+
+
+if __name__ == "__main__":
+    t0 = time.time()
+    session = requests.Session()
+    upMid = os.sys.argv[1]
+    up = getName(upMid)
+    uList, fList = [], []
+    if not os.path.exists(up[0]):
+        os.mkdir(up[0])
+    with open(f"{up[0]}/fans.csv", "w+") as f:
+        f.write("uid,mtime,uname,vipType,level,sex,sign\n")
+    for i in range(up[1]):
+        uList.extend(getFans(upMid, i))
+    pool = threadpool.ThreadPool(20)
+    tasks = threadpool.makeRequests(getInfo, uList)
+    [pool.putRequest(task) for task in tasks]
+    pool.wait()
+    print(f"Cost {time.time()-t0} secs")
+    print(f"{up[0]} Finished！")
