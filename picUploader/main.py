fea7ffe54f511ac156a937ef9b7b0200d300684a
brx86 <brx86@qq.com>
2021-11-06 15:24:01 +0800

    update

--- picUploader/main.py
+++ picUploader/main.py
@@ -1,12 +1,17 @@
 import os, requests
 from config import cookies
 
-
+# 定义上传函数
 def image_upload(file_path):
+    # api地址
     api_url = "https://api.vc.bilibili.com/api/v1/drawImage/upload"
+
+    # 打开图片文件
     with open(file_path, "rb") as f:
         img_file = f.read()
-    files = {"file_up": (file_name, img_file)}
+
+    # 设置post参数
+    files = {"file_up": (file_path, img_file)}
     data = {
         "biz": "draw",
         "category": "daily",
@@ -16,6 +21,8 @@ def image_upload(file_path):
         "Referer": "https://t.bilibili.com/",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0",
     }
+
+    # 向api发送post请求
     r = requests.post(
         api_url,
         files=files,
@@ -23,10 +30,17 @@ def image_upload(file_path):
         headers=headers,
         cookies=cookies,
         timeout=300,
-    ).json
-    img_url = r["data"]["image_url"]
-    print(f"Url: {img_url}")
-    return img_url
+    )
+
+    # 解析返回值，得到图片链接并请求api得到短链接
+    img_url = r.json()["data"]["image_url"]
+
+    r = requests.get(f"https://bili.fan/api/bili.php?b23tvurl={img_url}")
+    short_url = r.json()["data"]["content"]
+
+    # 输出结果
+    print(f"图片链接: {img_url}\n短网址:   {short_url}")
+    return img_url, short_url
 
 
 if __name__ == "__main__":
